9e6cdb86adb93fcc975dcf1b9cdbd0573327fba0
Adilma <adilmadoliveira@gmail.com>
2026-06-02 18:11:10 -0300

    adiciona self cheking ao demux1x8

--- Makefile
+++ Makefile
@@ -1,11 +1,18 @@
-.PHONY: run wave clean
+.PHONY: run run_self wave wave_self clean
 
 run:
 	iverilog -g2012 -o sim/demux.out rtl/demux1x8.v tb/demux1x8_tb.sv
 	vvp sim/demux.out
 
+run_self:
+	iverilog -g2012 -o sim/demux_self.out rtl/demux1x8.v tb/demux1x8_tb_selfcheck.sv
+	vvp sim/demux_self.out
+
 wave:
 	gtkwave sim/demux1x8_tb.vcd
+
+wave_self:
+	gtkwave sim/demux1x8_selfcheck.vcd
 
 clean:
 	rm -f sim/*.out sim/*.vcd
---
+++ tb/demux1x8_tb_selfcheck.sv
@@ -0,0 +1,107 @@
+//Function: The test bench applies random values to 3-bit select lines and checks the
+//dout. The waveform demux_tb.vcd can be observed using waveform viewer.
+//Test bench file: demux3x8_tb.v
+
+module demux1x8_tb;
+
+//Inputs
+logic clk;
+logic rstn;
+logic en;
+logic [2:0] sel;
+logic din;
+
+//outputs
+logic [7:0] dout;
+logic [7:0] expected;
+
+int match;
+int mismatch;
+int tr;
+
+// clock generation
+always #5 clk = ~clk; // toggle clock for every 5 ticks
+
+// Reference Model
+task refmod(
+    input logic en_i,
+    input logic [2:0] sel_i,
+    input logic din_i,
+    output logic [7:0] exp
+);
+begin
+    exp = 8'b00000000;
+
+    if (en_i)
+        exp[sel_i] = din_i;
+end
+endtask
+
+initial begin
+    clk = 0;
+    rstn = 0;
+    en = 0;
+    sel = 3'b000;
+    din = 1'b0;
+
+    expected = 8'b00000000;
+    match = 0;
+    mismatch = 0;
+    tr = 0;
+
+    $display("--------- Test Started ---------");
+        #10 rstn = 0;
+        #10 rstn = 1;
+
+
+    en = 1;
+
+        for (int i = 0; i < 8; i++) begin
+        sel = i[2:0];
+        din = 1'b1;
+
+        refmod(en, sel, din, expected);
+
+        @(posedge clk);
+        #1;
+
+        if (dout !== expected) begin
+            $display("FAIL: sel=%0d din=%0b expected=%b got=%b",
+                     sel, din, expected, dout);
+            mismatch++;
+        end else begin
+            $display("PASS: sel=%0d din=%0b expected=%b got=%b",
+                     sel, din, expected, dout);
+            match++;
+        end
+
+        tr++;
+    end
+
+    $display("\n======================");
+    $display("FINAL REPORT");
+    $display("======================");
+    $display("Transactions : %0d", tr);
+    $display("PASS         : %0d", match);
+    $display("FAIL         : %0d", mismatch);
+    $display("======================\n");
+
+    #20;
+    $finish;
+end
+
+demux1x8 uut (
+    .clk(clk),
+    .rstn(rstn),
+    .en(en),
+    .sel(sel),
+    .din(din),
+    .dout(dout)
+);
+
+initial begin
+    $dumpfile("sim/demux1x8_selfcheck.vcd");
+    $dumpvars(0, demux1x8_tb);
+end
+
+endmodule
